e043415a96c6c0a1b4409e00776981e62046ab55
KarthicRaj <skraj192002@gmail.com>
2014-07-03 17:28:41 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-dredd guestIdentity.md https://54Y6T02.dist.target.com/wcs/resources/store/10151/guestidentity
+dredd guestIdentity.md http://tws.target.com/wcs/resources/store/10151/guestidentity
 RESULT=$?
 exit $RESULT
